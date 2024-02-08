35f228cdf2479d7e3a30928ba26431d5b07a776f
ysmur <ysmur@yandex.ru>
2024-02-08 17:59:25 +0300

    1 самый простой сервер

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Привет, Яндекс!"
+
+@app.route('/index')
+def index1():
+    with open('templates/index.html', 'r', encoding='utf-8') as stream:
+        return stream.read()
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
