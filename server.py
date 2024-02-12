d77a4073c5ff300c8937b00f58b609ea83977eec
ysmur <ysmur@yandex.ru>
2024-02-12 17:16:38 +0300

    Урок 2. Задача 2 Тренировки в полёте

--- server.py
+++ server.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index(title):
     return render_template('base.html', title=title)
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
