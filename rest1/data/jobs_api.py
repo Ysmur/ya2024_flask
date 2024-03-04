ee022f24bc2420e32cf693f4800d8d390d6b5c7d
ysmur <ysmur@yandex.ru>
2024-03-04 17:56:45 +0300

    Rest1. Добавление одной работы в БД черезпередачу  json  по адресу http://127.0.0.1:5000/api/jobs и тестирование запросов в файле test.py

--- rest1/data/jobs_api.py
+++ rest1/data/jobs_api.py
@@ -1,5 +1,5 @@
 import flask
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request
 
 from . import db_session
 from .users import User
@@ -36,3 +36,20 @@ def get_one_jobs(jobs_id):
             'job': job.to_dict(only=('job', 'work_size', 'team_leader'))
         }
     )
+
+@blueprint.route('/api/jobs', methods=['POST'])
+def create_jobs():
+    if not request.json:
+        return make_response(jsonify({'error': 'Empty request'}), 400)
+    elif not all(key in request.json for key in
+                 ['team_leader', 'job', 'work_size']):
+        return make_response(jsonify({'error': 'Bad request'}), 400)
+    db_sess = db_session.create_session()
+    job = Job(
+        team_leader=request.json['team_leader'],
+        job=request.json['job'],
+        work_size=request.json['work_size']
+    )
+    db_sess.add(job)
+    db_sess.commit()
+    return jsonify({'id': job.id})
--- rest1/test.py
+++ rest1/test.py
@@ -1,6 +1,18 @@
-from requests import get
+from requests import get, post
 
 #print(get('http://localhost:5000/api/jobs').json())
 #print(get('http://localhost:5000/api/jobs/2').json())
 #print(get('http://localhost:5000/api/jobs/20').json())
-print(get('http://localhost:5000/api/jobs/t').json())
+#print(get('http://localhost:5000/api/jobs/t').json())
+
+#print(post('http://localhost:5000/api/jobs', json={}).json())
+
+# print(post('http://localhost:5000/api/jobs',
+#            json={'job': 'Заголовок'}).json())
+#
+print(post('http://localhost:5000/api/jobs',
+           json={'job': 'Заголовок',
+                 'team_leader': 1,
+                 'work_size': 1}).json())
+
+print(get('http://localhost:5000/api/jobs').json())
