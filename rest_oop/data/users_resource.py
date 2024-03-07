1dbb113333fab0d5b5168e1ae986482199b252a4
ysmur <ysmur@yandex.ru>
2024-03-07 18:55:58 +0300

    Rest_oop. три задачи классной работы

--- rest_oop/data/users_resource.py
+++ rest_oop/data/users_resource.py
@@ -3,6 +3,21 @@ from flask import jsonify
 from data import db_session
 from data.users import User
 from flask_restful import reqparse, abort, Api, Resource
+from werkzeug.security import generate_password_hash
+
+parser = reqparse.RequestParser()
+parser.add_argument('surname', required=True)
+parser.add_argument('name', required=True)
+parser.add_argument('age', required=True, type=int)
+parser.add_argument('position', required=True)
+parser.add_argument('speciality', required=True)
+parser.add_argument('address', required=True)
+parser.add_argument('email', required=True)
+parser.add_argument('hashed_password', required=True)
+
+
+def set_password(password):
+    return generate_password_hash(password)
 
 
 def abort_if_user_not_found(user_id):
@@ -14,7 +29,6 @@ def abort_if_user_not_found(user_id):
         abort(404, message=f"Users {user_id} not found")
 
 
-
 class UsersListResource(Resource):
     def get(self):
         session = db_session.create_session()
@@ -22,6 +36,23 @@ class UsersListResource(Resource):
         return jsonify({'users': [item.to_dict(
             only=('surname', 'name', 'age', 'position',
                   'speciality', 'address', 'email')) for item in users]})
+
+    def post(self):
+        args = parser.parse_args()
+        session = db_session.create_session()
+        user = User(
+            surname=args['surname'],
+            name=args['name'],
+            age=args['age'],
+            position=args['position'],
+            speciality=args['speciality'],
+            address=args['address'],
+            email=args['email'],
+            hashed_password=set_password(args['hashed_password'])
+        )
+        session.add(user)
+        session.commit()
+        return jsonify({'id': user.id})
 
 
 class UsersResource(Resource):
@@ -32,3 +63,11 @@ class UsersResource(Resource):
         return jsonify({'user': user.to_dict(
             only=('surname', 'name', 'age', 'position',
                   'speciality', 'address', 'email'))})
+
+    def delete(self, user_id):
+        abort_if_user_not_found(user_id)
+        session = db_session.create_session()
+        user = session.query(User).get(user_id)
+        session.delete(user)
+        session.commit()
+        return jsonify({'success': 'OK'})
--- rest_oop/test.py
+++ rest_oop/test.py
@@ -1,7 +1,24 @@
 from pprint import pprint
-from requests import get, post
+from requests import get, post, delete
 
 #pprint(get('http://localhost:5000/api/v2/users/2').json())
 #pprint(get('http://localhost:5000/api/v2/users/20').json())
-##????print(get('http://localhost:5000/api/v2/users/q').json())
+#pprint(get('http://localhost:5000/api/v2/users').json())
+# pprint(post('http://localhost:5000/api/v2/users',
+#            json={'surname': 'Markiz',
+#                  'name': 'Tom',
+#                  'age': 23,
+#                  'position': 'doctor',
+#                  'speciality': 'terapevt',
+#                  'address': 'module_1',
+#                  'email': 'ya@ya.ru',
+#                  'hashed_password': '123'}).json())
+
+# pprint(post('http://localhost:5000/api/v2/users',
+#             json={'surname': 'Markiz',
+#                   'name': 'Tom'}).json())
+
+print(delete('http://localhost:5000/api/v2/users/7').json())
+print(delete('http://localhost:5000/api/v2/users/70').json())
+
 pprint(get('http://localhost:5000/api/v2/users').json())
